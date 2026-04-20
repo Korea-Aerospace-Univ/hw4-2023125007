6978746603d52982dfde76e74e15e6d43b9765c1
2023125007 <min040325@gmail.com>
2026-04-20 15:03:30 +0900

    Add main function to compare input with answer

---
+++ p1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+    int answer = 0;
+    int insert = 0;
+    int count = 0;
+    scanf("%d", &answer);
+
+    do{
+        scanf("%d", &insert);
+        count += 1;
+
+        if(insert < answer){
+            printf("%d<?", insert);
+        }
+        else{
+            printf("%d>?", insert);
+        }
+    } while(insert != answer);
+
+    printf("%d==?\n", insert);
+    printf("%d", count);
+    return 0;
+}
